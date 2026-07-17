be2da2ae4754fb98302b5a32c9e9de8d8c331a2b
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-07-17 19:45:15 +0530

    feat: Implement PublicRoute component redirecting to dashboard if authenticated

--- src/routes/PublicRoute.tsx
+++ src/routes/PublicRoute.tsx
@@ -1,8 +1,10 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuthStore } from "../store/authStore";
 import Layout from "../app/layout";
 
 const PublicRoute = () => {
-  return <Layout><Outlet /></Layout>;
+  const token = useAuthStore((state) => state.token);
+  return token ? <Navigate to="/dashboard" replace /> : <Layout><Outlet /></Layout>;
 };
 
 export default PublicRoute;
