b8fc426b31bb1ad6628663b3d59a96bdda044991
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-07-17 19:45:14 +0530

    feat: Configure and integrate React Query Client Provider in App

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,36 @@ import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import { Toaster } from "react-hot-toast";
 import { SidebarProvider } from "./context/SidebarContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 
 function App() {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            retry: false,
+          },
+        },
+      })
+  );
+
   return (
-    <BrowserRouter>
-      <Toaster
-        position="top-right"
-        toastOptions={{
-          duration: 3000,
-        }}
-      />
-      <SidebarProvider>
-      <AppRoutes />
-      </SidebarProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            duration: 3000,
+          }}
+        />
+        <SidebarProvider>
+          <AppRoutes />
+        </SidebarProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
