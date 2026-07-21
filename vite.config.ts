d228c4a373ed7f5d69b195ee8eaab0b1f7947f47
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-07-21 20:20:24 +0530

    build: add sentryVitePlugin and enable sourcemaps in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "flyweis-technology-se",
+    project: "storage-material"
+  })],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
+
+  build: {
+    sourcemap: true
+  }
 });
